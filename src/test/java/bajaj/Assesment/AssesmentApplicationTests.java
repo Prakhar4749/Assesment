f4c7ca793c76c0783d284998f69122798ecbe706
Prakhar sakhare~ <prakharsakhare2226@gmail.com>
2025-09-22 15:49:34 +0530

    first commit

---
+++ src/test/java/bajaj/Assesment/AssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package bajaj.Assesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssesmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
